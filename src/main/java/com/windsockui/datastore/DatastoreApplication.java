a15379eb565015b2ec0d380d8ea8108e33d488eb
Mark van Wyk <mark@flyh2.com>
2020-04-02 18:01:10 +0200

    First commit

---
+++ src/main/java/com/windsockui/datastore/DatastoreApplication.java
@@ -0,0 +1,13 @@
+package com.windsockui.datastore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DatastoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DatastoreApplication.class, args);
+	}
+
+}
