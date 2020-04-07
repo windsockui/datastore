685a1c7fa702dd46a5d3d3b22a7a02225750e3f7
Mark van Wyk <mark@flyh2.com>
2020-04-07 15:39:14 +0200

    growing json stub

--- src/main/java/com/windsockui/datastore/config/Config.java
+++ src/main/java/com/windsockui/datastore/config/Config.java
@@ -3,9 +3,12 @@ package com.windsockui.datastore.config;
 import com.windsockui.datastore.entities.JsonData;
 import com.windsockui.datastore.repository.JsonDataRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.PostConstruct;
+import java.io.IOException;
 
 @Component
 public class Config {
@@ -17,29 +20,22 @@ public class Config {
     }
 
     @PostConstruct
-    public void populateData() {
+    public void populateData() throws IOException {
 
         if (jsonDataRepository.count() == 0) {
 
             JsonData data = new JsonData();
             data.setDomain("www.windsockui.com");
             data.setPath("/");
-            data.setJson("[\n" +
-                    "  {\n" +
-                    "    \"component\":\"heading\",\n" +
-                    "    \"data\": {\n" +
-                    "      \"value\": \"Welcome to windsockUI\"\n" +
-                    "    }\n" +
-                    "  },\n" +
-                    "  {\n" +
-                    "    \"component\":\"paragraph\",\n" +
-                    "    \"data\": {\n" +
-                    "      \"value\": \"This data was served to the components from a server.\"\n" +
-                    "    }\n" +
-                    "  }\n" +
-                    "]");
-            System.out.println(data);
-            jsonDataRepository.save(data);
+
+            try {
+                Resource resource = new ClassPathResource("/test-data.json");
+                data.setPath(resource.getInputStream().toString());
+                System.out.println(data);
+                jsonDataRepository.save(data);
+            } catch (IOException ioe) {
+                throw new IOException("Trying to load the example JSON stub (file) failed before being able to stick it in the database", ioe);
+            }
         }
     }
 }
